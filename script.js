55c4c1706656926fae44483498a2909cb97cb34f
PzOng <pangszee.ong@gmail.com>
2020-03-24 23:41:37 +1030

    api&currentdisplay

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var usersch = JSON.parse(localStorage.getItem("userhx")) || [];
 var city = 'adelaide';
 var apikey ='5e799cbc4834793851ed4eb3fbe95228';
-var key ='0959a518fd93cebc53ed34118e0e9471';
+
 
 
 
@@ -10,10 +10,10 @@ $(document).ready(function(){
     var cday = moment().format('dddd, Do MMM YYYY');
     $('#currentDay').text(cday);
     
-    var home = getinfo(city);
-    $('#currentCity').text(capitalisefst(city))
-    console.log(home, typeof home)
-
+    getinfo(city);
+    //$('#currentCity').text(capitalisefst(city))
+    
+    forcast(city);
 
 })
 
@@ -21,7 +21,7 @@ $(document).ready(function(){
 //ajax get weather info via API
 function getinfo(location){
 
-    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+location+'&units=metric&appid='+key
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+location+'&units=metric&appid='+apikey
     $.ajax({
           url: queryURL,
           method: "GET"
@@ -29,11 +29,12 @@ function getinfo(location){
             
             console.log(response);
             var info = response;
-            console.log(response, typeof response);
+            console.log(info, typeof info);
+            
 
             var lon = info.coord.lon
             var lat = info.coord.lat
-            var queryURL = 'http://api.openweathermap.org/data/2.5/uvi?appid='+key+'&lat='+lat+'&lon='+lon;
+            var queryURL = 'http://api.openweathermap.org/data/2.5/uvi?appid='+apikey+'&lat='+lat+'&lon='+lon;
             $.ajax({
                   url: queryURL,
                   method: "GET"
@@ -42,10 +43,52 @@ function getinfo(location){
                     console.log(response);
                     var uvinfo = response;
                     console.log(uvinfo, typeof uvinfo);
+                    displayinfo(info,uvinfo);
                   });
           });
   }
+
+  function forcast(location){
+    var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q='+location+'&units=metric&appid='+apikey
+    $.ajax({
+          url: queryURL,
+          method: "GET"
+          }).then(function(response) {
+            console.log(response);
+            var foreinfo = response;
+            console.log(foreinfo, typeof foreinfo);
+            
+            var date = foreinfo.list;
+            for(i=0;i<date.length;i++){
+              var x = date[i].dt;
+              var dateString = moment.unix(x).format('dddd, Do MMM YYYY');
+              console.log(dateString);
+            }
+            
+  })
+}
+function displayinfo(obj,obj1){
   
+  $('#currentCity').text(obj.name);
+  var x = Math.round(obj.main.temp)
+  $('#temp').text(x + ' °C');
+  var y = obj.weather[0].description;
+  $('#desc').text(capitalisefst(y));
+  $('#humid').text(obj.main.humidity + ' %');
+  $('#wind').text(obj.wind.speed + ' m/s');
+  
+  $('#uvi').text(obj1.value);
+  var q = Math.round(obj1.value);
+  var r = $('#uvi');
+  var s = $('#uvid');
+  if(q>=11){r.css('background-color','purple');s.text('Extreme')}
+  else if(q<=10 && q>=8){r.css('background-color','red');s.text('Very High')}
+  else if(q<=7 && q>=6){r.css('background-color','orange');s.text('High')}
+  else if(q<=5 && q>=3){r.css('background-color','yellow');s.text('Moderate')}
+  else if(q<=2 && q>=1){r.css('background-color','green');s.text('Low')}
+}
+
+
   //Capitalise 1st letter
   function capitalisefst(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
